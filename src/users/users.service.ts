a532102e51862c6e2d338269728ad6eda48e070e
laura-ferreira2 <laura.ferreira@ufba.br>
2025-10-07 17:53:34 -0300

    feat(users): add checkCredentials method to UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, ConflictException, InternalServerErrorException, UnprocessableEntityException } from '@nestjs/common';
+import {
+  Injectable,
+  ConflictException,
+  InternalServerErrorException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
@@ -6,6 +11,8 @@ import * as crypto from 'crypto';
 import { User } from './user.entity';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UserRole } from './enums';
+import { CredentialsDto } from './dtos/credentials.dto';
+
 
 @Injectable()
 export class UsersService {
@@ -13,8 +20,6 @@ export class UsersService {
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
   ) {}
-
-  
 
   // Criação de usuário ADMIN
   async createAdminUser(createUserDto: CreateUserDto): Promise<User> {
@@ -26,7 +31,10 @@ export class UsersService {
   }
 
   // Método genérico de criação de usuário
-  public async createUser(createUserDto: CreateUserDto, role: UserRole): Promise<User> {
+  public async createUser(
+    createUserDto: CreateUserDto,
+    role: UserRole,
+  ): Promise<User> {
     const { email, name, password } = createUserDto;
 
     const user = this.userRepository.create({
@@ -38,16 +46,42 @@ export class UsersService {
     });
 
     user.salt = await bcrypt.genSalt();
-    user.password = await bcrypt.hash(password, user.salt);
+    user.password = await this.hashPassword(password, user.salt);
 
     try {
-      return await this.userRepository.save(user);
+      const savedUser = await this.userRepository.save(user);
+      return savedUser;
     } catch (error: any) {
       if (error.code?.toString() === '23505') {
         throw new ConflictException('Endereço de email já está em uso');
       } else {
-        throw new InternalServerErrorException('Erro ao salvar o usuário no banco de dados');
+        throw new InternalServerErrorException(
+          'Erro ao salvar o usuário no banco de dados',
+        );
       }
     }
   }
+
+ async checkCredentials(credentialsDto: CredentialsDto): Promise<User | null> {
+    const { email, password } = credentialsDto;
+
+    const user = await this.userRepository.findOne({
+      where: { email, status: true },
+    });
+
+    //  método definido na entity
+    if (user && (await user.checkPassword(password))) {
+      return user;
+    }
+
+    return null;
+  }
+
+  // Função auxiliar para hashear a senha
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
+  }
+
+  
+
 }
